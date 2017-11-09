f6de0f164e28bbd4f1b5ae449531809178785f8e
unknown <neno.smg@hotmail.com>
2017-11-08 23:16:02 -0300

    IMC funcionando

--- src/pages/imc/imc.ts
+++ src/pages/imc/imc.ts
@@ -6,10 +6,23 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'imc.html',
 })
 export class ImcPage {
+  public num1:any;
+  public num2:any;
+  public result:any;
+  public saude:any;
 
-  calculo_Imc(): number{
-
-    return;
+  calculo_Imc(): void{
+    
+    this.result = Math.round( parseFloat(this.num2) / (parseFloat(this.num1) * parseFloat(this.num1))); 
+  if (this.result < 18){
+    this.saude = "Abaixo do peso.";
+  }else if (this.result > 18 && this.result < 25) {
+    this.saude = "Peso Normal.";
+  } else {
+    this.saude = "Acima do peso.";
+  }{
+    
+  }
   } 
 
   constructor(public navCtrl: NavController) {
